8ba91314deaa077387830663ace8ec7a77094ea3
nuthansai113 <130201140+nuthansai113@users.noreply.github.com>
2023-07-03 22:19:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+
+
+let element = document.getElementById("level");
+
+ let level = 0;
+
+ while(element && element.tagName !== "HTML"){
+   element = element.parentNode;
+   level++;
+ }
+
+ alert("The level of the element is: " + level);
